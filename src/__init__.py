e5d9ece5ec76b7ec7c03ddaa5c55a2e96230dbce
Bulalo_Coder <javemelchorodchigue2006@gmail.com>
2025-11-28 20:38:50 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,20 @@
+from .data_getter import DataGetter
 from .data_inspector import DataInspector
 from .data_doctor import DataDoctor
 from .report_generator import ReportGenerator
-from .data_getter import DataGetter  
+from .data_visualizer import DataVisualizer
+from .data_exporter import DataExporter
+from .data_pipeline import DataPipeline
 
-__all__ = ["DataInspector", "DataDoctor", "ReportGenerator", "DataGetter"] 
+__all__ = [
+    "DataGetter",
+    "DataInspector",
+    "DataDoctor",
+    "ReportGenerator",
+    "DataVisualizer",
+    "DataExporter",
+    "DataPipeline"
+]
 
 
 __version__ = "0.1.0"
