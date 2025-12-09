21c0f3411848976e9f3b2f09da8cb192a16ea321
Bulalo_Coder <javemelchorodchigue2006@gmail.com>
2025-12-09 19:28:16 +0800

    Bump version to 0.1.6

--- src/cleanops/__init__.py
+++ src/cleanops/__init__.py
@@ -3,7 +3,7 @@ from .data_preprocessor import DataInspector, DataCleaner, DataOrganizer
 from .data_output import DataExporter, ReportGenerator, DataOutput
 from .data_pipeline import DataPipeline
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 __all__ = [
     "DataGetter",
